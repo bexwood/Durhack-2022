9349be9daf9c07f234c4b958a50cd0fe297c5222
bexwood <bexewood@gmail.com>
2022-02-19 14:14:50 +0000

    added missing line to update screen

--- game.py
+++ game.py
@@ -8,4 +8,6 @@ screen = pygame.display.set_mode((width, height))
 background = pygame.image.load('Images/logo.png')
 
 while True:
+    screen.fill(0)
     screen.blit(background, (0,0))
+    pygame.display.flip()
